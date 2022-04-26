12ce6dcd5bc1491ba4be2ef90ea729561bfc8ced
mojtaba farzaneh <mojtabafarzaneh78@gmail.com>
2022-04-26 18:09:06 +0430

    promoting feature

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import url
 
 
 urlpatterns = [
-    url(r'^$', views.IndexPage.as_view(), name='index')
+    url(r'^$', views.IndexPage.as_view(), name='index'),
+    url(r'^contact$', views.ContactPage.as_view(), name='contact')
+
+
 ]
--- blog/views.py
+++ blog/views.py
@@ -17,9 +17,27 @@ class IndexPage(TemplateView):
                 'category': article.category.title,
 
             })
+        
+        promote_data = []
+        all_articles_promote = Article.objects.filter(promote=True)
+
+        for article in all_articles_promote:
+            promote_data.append({
+                'title': article.title,
+                'cover': article.cover.url if article.cover else None,
+                'created_at': article.created_at.date(),
+                'category': article.category.title,
+                'author': article.author.user.first_name + ' ' + article.author.user.last_name,
+                'avatar': article.author.avatar.url if article.author.avatar else None,
+
+            })
 
         context = {
             'article_data': article_data,
+            'promote_data': promote_data,
         }
 
         return render(request, 'index.html', context)
+
+class ContactPage(TemplateView):
+    template_name='contact.html'
